8442067d05c30a9e28923918da3ab74bb9ef04ec
Kevin Ward <antiomiae@gmail.com>
2015-06-25 08:20:10 -0500

    We can lint now.

--- gulpfile.js
+++ gulpfile.js
@@ -10,23 +10,24 @@ var del = require('del');
 var connect = require('gulp-connect');
 var bower = require('gulp-bower');
 var wiredep = require('wiredep').stream;
+var jshint = require('gulp-jshint');
 
 var dir = {
-	index: './app/index.html',
-	sourceDir: './app',
-	source: ['./app/**/*.js', '!./app/{lib,lib/**}'],
-	main: './app/main.js',
-	lib: ['!*.json',
-		'./app/lib/**/*.*',
-		//'./node_modules/systemjs/dist/system.*',
-		//'./node_modules/es6-module-loader/dist/es6-module-loader.*',
+    index: './app/index.html',
+    sourceDir: './app',
+    source: ['./app/**/*.js', '!./app/{lib,lib/**}'],
+    main: './app/main.js',
+    lib: ['!*.json',
+        './app/lib/**/*.*',
+        //'./node_modules/systemjs/dist/system.*',
+        //'./node_modules/es6-module-loader/dist/es6-module-loader.*',
         'node_modules/es6-micro-loader/dist/system-polyfill.js'
-	],
-	libOut: './build/static/lib',
-	build: './build',
-	targetAppJs: 'app.js',
-	img: './app/assets/img/**/*.png',
-	imgOut: './build/static/img'
+    ],
+    libOut: './build/static/lib',
+    build: './build',
+    targetAppJs: 'app.js',
+    img: './app/assets/img/**/*.png',
+    imgOut: './build/static/img'
 };
 
 var staticCopyTasks = [];
@@ -37,61 +38,71 @@ copyTask('img');
 copyTask('lib');
 
 gulp.task('clean-build', function(cb) {
-	del(dir.build, cb);
+    del(dir.build, cb);
 });
 
 gulp.task('copy-static', staticCopyTasks);
 
+gulp.task('lint', function() {
+    return gulp.src(dir.source, {
+            base: dir.sourceDir
+        })
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'));
+});
+
 gulp.task('compileApp', function() {
-	var babelOptions = {
-		modules: 'system',
-		moduleIds: true
-	};
+    var babelOptions = {
+        modules: 'system',
+        moduleIds: true
+    };
 
 
-	return gulp.src(dir.source, {
-			base: dir.sourceDir
-		})
-		.pipe(plumber())
-		.pipe(sourceMaps.init())
-		.pipe(babel(babelOptions))
+    return gulp.src(dir.source, {
+            base: dir.sourceDir
+        })
+        .pipe(plumber())
+        .pipe(sourceMaps.init())
+        .pipe(babel(babelOptions))
         .pipe(concat('all.js'))
-        .pipe(sourceMaps.write('.', {sourceMappingURLPrefix: '/'}))
+        .pipe(sourceMaps.write('.', {
+            sourceMappingURLPrefix: '/'
+        }))
         .pipe(gulp.dest(dir.build));
 });
 
 gulp.task('watch', function() {
-	gulp.watch([dir.source, dir.lib, dir.index, 'gulpfile.js'], ['build-dev']);
+    gulp.watch([dir.source, dir.lib, dir.index, 'gulpfile.js'], ['build-dev']);
 });
 
 gulp.task('connect', function() {
-	connect.server({
-		root: 'build',
-		livereload: false,
-		host: '0.0.0.0'
-	});
+    connect.server({
+        root: 'build',
+        livereload: false,
+        host: '0.0.0.0'
+    });
 });
 
 gulp.task('build-dev', function() {
-	runSeq('clean-build', ['copy-static', 'compileApp'], 'bower');
+    runSeq('clean-build', ['copy-static', 'compileApp'], 'bower');
 });
 
 gulp.task('default', function() {
-	runSeq('build-dev', ['watch', 'connect']);
+    runSeq('build-dev', ['watch', 'connect']);
 });
 
 gulp.task('bower', function() {
-	gulp.src(dir.build + '/index.html')
-		.pipe(wiredep({
-			cwd: '.',
-			ignorePath: /\.\.\/app\//,
-			fileTypes: {
-				html: {
-					replace: {
-						js: '<script src="static/{{filePath}}"></script>'
-					}
-				}
-			}
-		}))
-		.pipe(gulp.dest(dir.build));
+    gulp.src(dir.build + '/index.html')
+        .pipe(wiredep({
+            cwd: '.',
+            ignorePath: /\.\.\/app\//,
+            fileTypes: {
+                html: {
+                    replace: {
+                        js: '<script src="static/{{filePath}}"></script>'
+                    }
+                }
+            }
+        }))
+        .pipe(gulp.dest(dir.build));
 });
